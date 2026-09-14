6202bb5ac3ab3dcea00faea079e64532776cacd3
haoran <lxhao61@gmail.com>
2026-09-14 10:25:26 +0800

    Add files via upload

--- op3.sh
+++ op3.sh
@@ -9,17 +9,3 @@
 # File name: diy-op3.sh
 # Description: OpenWrt DIY script part 2 (rm Update feeds)
 #
-
-#=================================================
-#sudo rm -rf feeds/packages/net/sing-box
-#sudo rm -rf feeds/packages/net/microsocks
-#sudo rm -rf feeds/packages/net/trojan-go
-sudo rm -rf feeds/packages/net/v2raya
-git clone https://github.com/orgx2812/v2raya feeds/packages/net/v2raya
-#sudo rm -rf feeds/packages/net/v2ray-core
-sudo rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/orgx2812/v2ray-geodata feeds/packages/net/v2ray-geodata
-sudo rm -rf feeds/packages/net/xray-core
-git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
-#sudo rm -rf feeds/packages/net/chinadns-ng
-#sudo rm -rf feeds/luci/applications/luci-app-homeproxy
